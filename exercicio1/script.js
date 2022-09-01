612f028eeaa7d384d102f852cd930481ea1a7c08
Clara <claradsfcarneiro@gmail.com>
2022-09-01 14:22:22 -0300

    Realizar exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeros = [2, 6, 1999];
+const strings = ["Clara", "Katia", "Evandro"];
+const stringsNumerosEBooleanos = ["Clara","Katia", "Evandro", 23, 1999 + 23 === 2022];
+
+console.log(numeros);
+console.log(strings);
+console.log(stringsNumerosEBooleanos);
